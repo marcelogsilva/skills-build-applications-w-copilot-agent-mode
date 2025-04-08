31f31bc70c29eab6cddb0a48ab06602df72411b5
marcelogsilva <82273820+marcelogsilva@users.noreply.github.com>
2025-04-08 18:10:55 +0000

    Initial commit for OctoFit Tracker backend setup

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .models import User, Team, Activity, Leaderboard, Workout
 from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
 from django.conf import settings
+from rest_framework.response import Response
 
 class APIEndpointSuffixMixin:
     def get_api_suffix(self):
