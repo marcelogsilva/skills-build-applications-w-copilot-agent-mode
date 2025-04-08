76f2547d9fe975b39712b2b5b6938f10db8fa013
marcelogsilva <82273820+marcelogsilva@users.noreply.github.com>
2025-04-08 17:32:52 +0000

    Initial commit for OctoFit Tracker backend setup

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--p5qo64-+8$r6^8@xo@!rxs*$cs@ul3u+kocx(^tlzo-&tcv&g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev']
 
 
 # Application definition
