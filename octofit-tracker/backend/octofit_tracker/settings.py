8bfaa85b5e062f3758cff1198192887b9a76ee95
marcelogsilva <82273820+marcelogsilva@users.noreply.github.com>
2025-04-08 18:23:16 +0000

    Initial commit for OctoFit Tracker backend setup

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -101,6 +101,15 @@ CORS_ALLOW_HEADERS = [
     'x-csrftoken',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
